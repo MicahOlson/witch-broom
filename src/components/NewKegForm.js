0d97b2db562cad80cd45e5ca9886fc13782c8d44
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 14:34:09 -0700

    Initialize pintCount on construction

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -11,6 +11,7 @@ function NewKegForm(props) {
       brand: event.target.brand.value,
       price: event.target.price.value,
       alcoholContent: event.target.alcoholContent.value,
+      pintCount: 124,
       id: v4()
     });
   }
