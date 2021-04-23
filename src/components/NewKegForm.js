d6ce5bb69495c5f52d46ed80589fb1bf3eb06310
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 10:12:54 -0700

    Add event handler to NewKegForm

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReusableForm from './ReusableForm';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
 
-function NewKegForm() {
+function NewKegForm(props) {
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.price.value,
+      alcoholContent: event.target.alcoholContent.value,
+      id: v4()
+    });
+  }
   return (
     <>
       <ReusableForm
@@ -10,5 +22,9 @@ function NewKegForm() {
     </>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
 
 export default NewKegForm;
