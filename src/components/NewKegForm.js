ed5bb4fcf94c22518e2183eb2c8bf53a85bae691
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 09:57:52 -0700

    Create reusable form and feed NewKegForm

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+
+function NewKegForm() {
+  return (
+    <>
+      <ReusableForm
+        buttonText="Add Keg"
+      />
+    </>
+  );
+}
+
+export default NewKegForm;
