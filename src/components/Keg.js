f5e4c8fe3299b2322a86b19d35b7c578713cbf47
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 10:51:06 -0700

    Invoke whenKegClicked on keg click

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 function Keg(props) {
   return (
     <>
-      <h3>{props.name}</h3>
-      <h4>from {props.brand}</h4>
-      <p>Price: ${props.price}</p>
-      <p>ABV: {props.alcoholContent}%</p>
-      <hr/>
+      <div onClick={() => props.whenKegClicked(props.id)}>
+        <h3>{props.name}</h3>
+        <h4>from {props.brand}</h4>
+        <p>Price: ${props.price}</p>
+        <p>ABV: {props.alcoholContent}%</p>
+        <hr/>
+      </div>
     </>
   );
 }
@@ -17,7 +19,9 @@ Keg.propTypes = {
   name: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.number,
-  alcoholContent: PropTypes.number
-}
+  alcoholContent: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
+};
 
 export default Keg;
