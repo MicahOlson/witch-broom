fc8f5a5407f65cd6219503d8b38d061022e27448
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 09:43:31 -0700

    Add looping of temporary keg list constant

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import Keg from './Keg';
 
+const mainKegList = [
+  {
+    name: "Skeleton Cruise",
+    brand: "Hop Butcher for the World",
+    price: "40.50",
+    alcoholContent: "6.25"
+  },
+  {
+    name: "Cold IPA",
+    brand: "Ecliptic Brewing",
+    price: "45.00",
+    alcoholContent: "8.00"
+  }
+]
+
 function KegList() {
   return (
     <>
-      <Keg 
-        name="Skeleton Cruise"
-        brand="Hop Butcher for the World"
-        price="40.50"
-        alcoholContent="6.25"
-      />
-      <Keg 
-      name="Cold IPA"
-      brand="Ecliptic Brewing"
-      price="45.00"
-      alcoholContent="8.00"
-      />
+      {mainKegList.map((keg, index) => 
+        <Keg 
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          alcoholContent={keg.alcoholContent}
+          key={index}
+        />
+      )}
     </>
   );
 }
