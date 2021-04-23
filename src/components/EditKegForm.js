8f852c9ccfbe70ea6cf0725a1fd9c45b16657e77
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 11:27:41 -0700

    Add EditKegForm component

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+
+function EditKegForm(props) {
+  return (
+    <>
+      <ReusableForm
+        buttonText="Update Details"
+      />
+    </>
+  );
+}
+
+export default EditKegForm;
