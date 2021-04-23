5ab6c993525e4d8310202deef2d09cc9ac8d8a25
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 10:47:42 -0700

    Pass props through KegList component

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -5,13 +5,15 @@ import PropTypes from 'prop-types';
 function KegList(props) {
   return (
     <>
-      {props.kegList.map((keg, index) => 
+      {props.kegList.map((keg) => 
         <Keg 
+          whenKegClicked={props.onKegSelection}
           name={keg.name}
           brand={keg.brand}
           price={keg.price}
           alcoholContent={keg.alcoholContent}
-          key={index}
+          id={keg.id}
+          key={keg.id}
         />
       )}
     </>
@@ -19,7 +21,8 @@ function KegList(props) {
 }
 
 KegList.propTypes = {
-  kegList: PropTypes.array
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
 };
 
 export default KegList;
