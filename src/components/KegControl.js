8a5a9c7058698edb19fcb5efd31e940c3e33fe8f
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 09:58:19 -0700

    Add KegControl to manage state

---
+++ src/components/KegControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewKegForm from './NewKegForm';
+import KegList from './KegList';
+
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default KegControl;
