ddb14b0061626d52a2eb5a18528f7b3aaffd4d1c
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-23 14:35:31 -0700

    Refactor horizontal rule to top of group

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -5,11 +5,11 @@ function Keg(props) {
   return (
     <>
       <div onClick={() => props.whenKegClicked(props.id)}>
+        <hr/>
         <h3>{props.name}</h3>
-        <h4>from {props.brand}</h4>
+        <h4>{props.brand}</h4>
         <p>Price: ${props.price}</p>
         <p>ABV: {props.alcoholContent}%</p>
-        <hr/>
       </div>
     </>
   );
